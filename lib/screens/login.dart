59434945fe923f4eb8f5e79f5476a93102094979
asaqa988 <asaqa001@gmail.com>
2022-09-29 15:05:54 +0300

    u

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
+import 'package:myapp/screens/yourpage.dart';
 
 class Log_in extends StatefulWidget {
   const Log_in({super.key});
@@ -9,85 +10,95 @@ class Log_in extends StatefulWidget {
 }
 
 class _Log_inState extends State<Log_in> {
+  TextEditingController userInput = TextEditingController();
+  String myText = "";
+  String mypassword = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Log in page")),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            CircleAvatar(
-              radius: 100,
-              child: Image(image: AssetImage("images/2.png")),
+        child: Column(children: [
+          CircleAvatar(
+            radius: 100,
+            child: Image(image: AssetImage("images/2.png")),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: TextFormField(
+              onChanged: ((value) {
+                myText = value.toString();
+                // if (myText == "asaqa001@gmail.com") {
+                //   showDialog(
+                //       context: context,
+                //       builder: (context) {
+                //         return AlertDialog(
+                //           content: Text("email was found"),
+                //         );
+                //       });
+                // }
+              }),
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green, width: 1)),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  hintText: "Please enter your email",
+                  label: Text(
+                    "Email",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  prefixIcon: Icon(Icons.people)),
+              maxLines: 1,
+              maxLength: 20,
+              textAlign: TextAlign.center,
             ),
-            Padding(
-              padding: const EdgeInsets.all(50.0),
-              child: TextField(
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green, width: 1)),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    hintText: "Please enter your email",
-                    label: Text(
-                      "Email",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                    prefixIcon: Icon(Icons.people)),
-                maxLines: 1,
-                maxLength: 20,
-                textAlign: TextAlign.center,
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: TextFormField(
+              onChanged: ((value) {
+                mypassword = value.toString();
+              }),
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green, width: 1)),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  hintText: "Please enter your password",
+                  label: Text(
+                    "password",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  prefixIcon: Icon(Icons.people)),
+              maxLines: 1,
+              maxLength: 20,
+              textAlign: TextAlign.center,
+              obscureText: true,
             ),
-            Padding(
-              padding: const EdgeInsets.all(50.0),
-              child: TextField(
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green, width: 1)),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    hintText: "Please enter your password",
-                    label: Text(
-                      "password",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                    prefixIcon: Icon(Icons.people)),
-                maxLines: 1,
-                maxLength: 20,
-                textAlign: TextAlign.center,
-                obscureText: true,
-              ),
-            ),
-            ElevatedButton(
-                onPressed: () {
+          ),
+          ElevatedButton(
+              onPressed: () {
+                if (myText == "asaqa001@gmail.com" && mypassword == "123") {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return your_page();
+                  }));
+                } else if (myText != "asaqa001@gmail.com" &&
+                    mypassword != "123") {
                   showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        content: SingleChildScrollView(
-                          scrollDirection: Axis.horizontal,
-                          child: Row(
-                            children: [
-                              Text(
-                                  "Can you please wait until we check your credentials?"),
-                              Icon(Ionicons.logo_android)
-                            ],
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          content: Center(
+                            child: Text("sorry wrong email or password"),
                           ),
-                        ),
-                        actions: [
-                          ElevatedButton(
-                              onPressed: () {}, child: Text("I wil wait")),
-                          ElevatedButton(
-                              onPressed: () {}, child: Text("no i can't wait"))
-                        ],
-                      );
-                    },
-                  );
-                },
-                child: Text("log-in"))
-          ],
-        ),
+                        );
+                      });
+                }
+              },
+              child: Text("log-in")),
+        ]),
       ),
     );
   }
---
+++ lib/screens/yourpage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class your_page extends StatelessWidget {
+  const your_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
